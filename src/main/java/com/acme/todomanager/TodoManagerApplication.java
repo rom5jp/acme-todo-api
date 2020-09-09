c091df172e8d7d5976ab1c91b0ff9404ffd56b38
Romero Medeiros <romeromedeiros5@gmail.com>
2020-09-09 00:48:44 -0300

    Adds CORS configuration.

--- src/main/java/com/acme/todomanager/TodoManagerApplication.java
+++ src/main/java/com/acme/todomanager/TodoManagerApplication.java
@@ -2,12 +2,13 @@ package com.acme.todomanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TodoManagerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TodoManagerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TodoManagerApplication.class, args);
+    }
 }
