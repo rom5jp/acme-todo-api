cfa4c940df1e7481b2ea110591925cd8ae5ca398
Romero Medeiros <romeromedeiros5@gmail.com>
2020-09-08 22:20:48 -0300

    Implements UPDATE and DELETE actions.

--- src/main/java/com/acme/todomanager/controller/TaskController.java
+++ src/main/java/com/acme/todomanager/controller/TaskController.java
@@ -3,12 +3,13 @@ package com.acme.todomanager.controller;
 import com.acme.todomanager.entity.Task;
 import com.acme.todomanager.repository.ITaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class TaskController {
@@ -23,5 +24,30 @@ public class TaskController {
     @RequestMapping(value = "tasks", method = RequestMethod.POST)
     public Task Create(@RequestBody Task task) {
         return this.repository.save(task);
+    }
+
+    @RequestMapping(value = "tasks/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Task> Update(@PathVariable("id") Long id, @PathParam("done") boolean done) {
+        Optional<Task> fetchedTask = this.repository.findById(id);
+
+        if (fetchedTask.isPresent()) {
+            Task updatedTask = fetchedTask.get();
+            updatedTask.setDone(done);
+            this.repository.save(updatedTask);
+            return new ResponseEntity<>(updatedTask, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @RequestMapping(value = "tasks/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Task> Delete(@PathVariable("id") Long id) {
+        Optional<Task> fetchedTask = this.repository.findById(id);
+        if (fetchedTask.isPresent()) {
+            this.repository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
