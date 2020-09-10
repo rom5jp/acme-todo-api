1dfc7efbd5bac6a9582f7fa106cabe06d161f914
Romero Medeiros <romeromedeiros5@gmail.com>
2020-09-10 20:54:21 -0300

    Improves UPDATE action.

--- src/main/java/com/acme/todomanager/controller/TaskController.java
+++ src/main/java/com/acme/todomanager/controller/TaskController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -26,17 +27,18 @@ public class TaskController {
         return this.repository.save(task);
     }
 
-    @RequestMapping(value = "tasks/{id}", method = RequestMethod.PUT)
-    public ResponseEntity<Task> Update(@PathVariable("id") Long id, @PathParam("done") boolean done) {
+    @PutMapping("tasks/{id}")
+    public ResponseEntity<Task> Update(@PathVariable("id") Long id, @RequestBody Task task) {
         Optional<Task> fetchedTask = this.repository.findById(id);
 
-        if (fetchedTask.isPresent()) {
-            Task updatedTask = fetchedTask.get();
-            updatedTask.setDone(done);
-            this.repository.save(updatedTask);
-            return new ResponseEntity<>(updatedTask, HttpStatus.OK);
-        } else {
+        if (!id.equals(task.getId())) {
+            return new ResponseEntity("The PathVariable ID must be equals to the entity ID.", HttpStatus.BAD_REQUEST);
+        }
+        else if (fetchedTask.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            this.repository.save(task);
+            return new ResponseEntity<>(task, HttpStatus.OK);
         }
     }
 
